4a64c4376825ffb90c3773b90a47f8095076be2f
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-08-31 19:22:25 +0300

    Update Education.jsx

--- src/modules/MainInform/components/Education/Education.jsx
+++ src/modules/MainInform/components/Education/Education.jsx
@@ -18,18 +18,17 @@ const Education = () => {
           <h4 className={style.timelineItemTitle}>
             GoIT - start your career in IT
           </h4>
-          <span className={style.timelineItemYear}>2023-2024</span>
+          <span className={style.timelineItemYear}>2024-2025</span>
           <p className={style.timelineItemPosition}>Full Stack Developer</p>
         </li>
 
         <li className={style.timelineItem}>
           <h4 className={style.timelineItemTitle}>
-            The Donbas State Engineering Academy
+            Poltava V.G. Korolenko National Pedagogical University
           </h4>
-          <span className={style.timelineItemYear}>2016-2018</span>
+          <span className={style.timelineItemYear}>1996-2001</span>
           <p className={style.timelineItemPosition}>
-            Master`s degree in Information Technology in Engineering and
-            Business
+            Faculty of Natural Sciences and Management
           </p>
         </li>
       </ul>
