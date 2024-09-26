5d0408293939cfa014043ec7d25a02425deaf019
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-09-26 19:18:27 +0300

    Update recommendations.js

--- src/shared/data/recommendations.js
+++ src/shared/data/recommendations.js
@@ -1,38 +1,36 @@
-import {
-    imagesPhoto
-} from 'shared/images';
+import { imagesPhoto } from 'shared/images';
 
 const recommendations = [
-    {
-        id: 1,
-        name: 'Olha Katrych',
-        position: 'Frontend Developer (HTML5, CSS3, JavaScript, React)',
-        link: 'https://www.linkedin.com/in/olha-katrych/',
-        img1x: `${imagesPhoto.olha}`,
-        img2x: `${imagesPhoto.olha2x}`,
-        date: '21 April, 2024',
-        post: 'I would like to recommend Krystyna as the specialist with the height skills of the leadership as we have worked together in the project creating the web-site using the following technologies: HTML, CSS, JavaScript where Krystyna was the Teamleader. I want to take into account that Krystyna is amiable, attentive to the details and needs of each participent of the team.She has the knowledge at a decent level according to all of the mentioned technologies above. I recommend Krysytyna for the future projects and I would like to work with her again with the big pleasure.I was glad to be the part of your team. Thank you for your job and invaluable contribution to the overall project.'
-    },
-    {
-        id: 2,
-        name: 'Anna Bezkorovaina',
-        position: 'Frontend Developer (HTML5, CSS3, JavaScript)',
-        link: 'https://www.linkedin.com/in/annabezkorovaina/',
-        img1x: `${imagesPhoto.anna}`,
-        img2x: `${imagesPhoto.anna2x}`,
-        date: '05 April, 2024',
-        post: 'I highly recommend Krystyna as both an outstanding team leader and a skilled developer. Throughout our collaboration on two team projects, Krystyna served as the team lead, demonstrating exceptional leadership qualities. In these projects, which involved HTML, CSS, and JavaScript development, Krystyna effectively managed the development process and motivated the team to achieve our goals. Thanks to her leadership, both projects were successfully completed and delivered.'
-    },
-    {
-        id: 3,
-        name: 'Svitlana Miroshnychenko',
-        position: 'Fullstack Developer (HTML5, CSS3, JavaScript, React, Node)',
-        link: 'https://www.linkedin.com/in/svitlana-miroshnychenko-12659a2b6/',
-        img1x: `${imagesPhoto.svitlana}`,
-        img2x: `${imagesPhoto.svitlana2x}`,
-        date: '03 March, 2024',
-        post: 'I worked on two team projects with Kristina, where she served as a team lead in both. The first project involved development using HTML and CSS, while the second one included the use of HTML, CSS, and JS. In both cases, collaborating with Kristina clearly demonstrated her ability to lead the team. Her role as a team lead was very clear; she adeptly managed the development process and motivated the team to achieve goals.'
-    }
-]
+  {
+    id: 1,
+    name: 'Olha Katrych',
+    position: 'Frontend Developer (HTML5, CSS3, JavaScript, React)',
+    link: 'https://www.linkedin.com/',
+    img1x: `${imagesPhoto.olha}`,
+    img2x: `${imagesPhoto.olha2x}`,
+    date: '21 April, 2024',
+    post: 'I would like to recommend as the specialist with the height skills of the leadership as we have worked together in the project creating the web-site using the following technologies: HTML, CSS, JavaScript where Krystyna was the Teamleader. I want to take into account that Krystyna is amiable, attentive to the details and needs of each participent of the team.She has the knowledge at a decent level according to all of the mentioned technologies above. I recommend Krysytyna for the future projects and I would like to work with her again with the big pleasure.I was glad to be the part of your team. Thank you for your job and invaluable contribution to the overall project.',
+  },
+  {
+    id: 2,
+    name: 'Anna',
+    position: 'Frontend Developer (HTML5, CSS3, JavaScript)',
+    link: 'https://www.linkedin.com/',
+    img1x: `${imagesPhoto.anna}`,
+    img2x: `${imagesPhoto.anna2x}`,
+    date: '05 April, 2024',
+    post: 'I highly recommend as both an outstanding team leader and a skilled developer. Throughout our collaboration on two team projects, Krystyna served as the team lead, demonstrating exceptional leadership qualities. In these projects, which involved HTML, CSS, and JavaScript development, Krystyna effectively managed the development process and motivated the team to achieve our goals. Thanks to her leadership, both projects were successfully completed and delivered.',
+  },
+  {
+    id: 3,
+    name: 'Svitlana',
+    position: 'Fullstack Developer (HTML5, CSS3, JavaScript, React, Node)',
+    link: 'https://www.linkedin.com/',
+    img1x: `${imagesPhoto.svitlana}`,
+    img2x: `${imagesPhoto.svitlana2x}`,
+    date: '03 March, 2024',
+    post: 'I worked on two team projects with, where she served as a team lead in both. The first project involved development using HTML and CSS, while the second one included the use of HTML, CSS, and JS. In both cases, collaborating with Kristina clearly demonstrated her ability to lead the team. Her role as a team lead was very clear; she adeptly managed the development process and motivated the team to achieve goals.',
+  },
+];
 
-export default recommendations
+export default recommendations;
